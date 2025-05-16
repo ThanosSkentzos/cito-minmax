47687f63ec6914d5bd7134de17c8b40d528d7817
ThanosSkentzos <kalligerist@gmail.com>
2025-05-16 17:02:46 +0200

    sinograms

--- sino.py
+++ sino.py
@@ -7,7 +7,7 @@ from utils import load_file
 from scipy.ndimage import zoom
 BRONCHI_LEVEL = 1
 LUNG_LEVEL = 0.3
-FACTOR = 1
+FACTOR = 4
 def upscale_z(volume, factor=2, order=1):
     if volume.ndim != 3:
         raise ValueError("Input volume must be a 3D array (X, Y, Z)")
@@ -29,39 +29,54 @@ def make_volume(bro_filename,lung_filename,bro_level=BRONCHI_LEVEL,lung_level=LU
     volume = np.zeros(shape).astype(float)
     volume[*lung.T] = lung_level            # in xyz
     volume[*bro.T] = bro_level              # in xyz [100,100,100]
-    #TODO downscale across xy 
-    dvolume = downsample_z_average(volume,FACTOR)   # in [100,100,50]
-    #TODO upscale
-    volume_LD = upscale_z(dvolume,FACTOR)
-    volume_LD = volume_LD.transpose([2,1,0])      # xyz -> zyx for astra
-    return volume_LD
+    # volume = downsample_z_average(volume,FACTOR)   # in [100,100,50]
+    # volume_LD = upscale_z(volume,FACTOR)
+    return volume
 def make_astra_volume(volume):
-    z,y,x = volume.shape
-    vol = astra.create_vol_geom(x,y,z)
+    x,y,z = volume.shape
+    vol = astra.create_vol_geom(y,z,x)   #takes rows,cols,slices
     phantom_id = astra.data3d.create('-vol',vol,volume)
     return vol,phantom_id
-def make_projection(type,num_detectors,detector_width,
+def make_projection(type,num_detectors_X,num_detectors_Y,spacingX,spacingY,
                     start_radian,end_radian,amount,include_end=False):
     angles = np.linspace(start_radian, end_radian, amount,include_end)
-    proj_geom = astra.create_proj_geom(type, detector_width, detector_width, num_detectors, num_detectors, angles)
+    proj_geom = astra.create_proj_geom(type, spacingX, spacingY, num_detectors_X, num_detectors_Y, angles)
     return proj_geom
 # %%
-def make_sinograms():
-    volume = make_volume("bro.npy","lung.npy")
+def make_sinograms(volume,numX,numY,scale):
+    x,y,z=0,1,2
+    volume = volume.transpose([z,x,y])
     vol,phantom_id = make_astra_volume(volume)
-    proj_geom = make_projection("parallel3d",100,1,0,2*np.pi,180,True)
+    spacingx = 1
+    spacingy = 1
+    proj_geom = make_projection("parallel3d",numX,numY,spacingx,spacingy,0,2*np.pi,180,True)
     id,sino = astra.creators.create_sino3d_gpu(volume,proj_geom,vol)
     sino = sino.transpose([1,0,2])
-    return sino
+    angles,z,r = sino.shape
+    s = sino.reshape(angles,z//scale,scale,r).mean(axis=2)
+    return s
 #%%
-if __name__=="__main__":
+def main():
     import matplotlib.pyplot as plt
-    sino = make_sinograms()
-    for i in range(0,180,4):
-        s = sino.flatten()
-        plt.imshow(sino[i,:,:],cmap='gray')
-        plt.pause(0.001)
+    volume = make_volume("bro.npy","lung.npy")
+    # volume = volume[:-2,:-1,:]
+    for scale in [1,2,4,5,10]:
+        sino= make_sinograms(volume,100,100,scale)
+        plt.figure()
+        print(sino.shape)
+        angles,z,r = sino.shape
+        print(z//scale//2)
+        plt.imshow(sino[:,z//2,:],cmap='gray')
+    plt.show()
+    # plt.figure()
+    # for i in range(0,180,4):
+        # plt.imshow(s[i,:,:],cmap='gray')
+        # plt.pause(0.1)
+    # plt.show()
+if __name__=="__main__":
+    main()
 # %%
 #NEXT STEPS 
 # generate data by downscaling
 # train neural network find image2image or use MS-D like paper with 100 layers
+# %%
